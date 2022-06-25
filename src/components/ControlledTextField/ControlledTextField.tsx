4be45f1bb31668e832b287f7206a04e8be4d7bf3
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-06-25 21:12:49 +0700

    cso

--- src/components/ControlledTextField/ControlledTextField.tsx
+++ src/components/ControlledTextField/ControlledTextField.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react"
-import { ITextFieldProps, TextField } from "@fluentui/react"
+import { ITextFieldProps, ITextFieldStyles, TextField } from "@fluentui/react"
 import { Controller } from "react-hook-form"
 import { HookFormProps } from "../../app/HookFormProps"
+
 
 export const ControlledTextField: FC<HookFormProps & ITextFieldProps> = (props) => {
     return (
@@ -22,6 +23,7 @@ export const ControlledTextField: FC<HookFormProps & ITextFieldProps> = (props) 
                     name={fieldName}
                     errorMessage={error && error.message}
                     defaultValue={undefined}
+                    required={props.required}                
                 />
             )}  
         />
--- src/components/PersonFormulir/PersonFormulir.tsx
+++ src/components/PersonFormulir/PersonFormulir.tsx
@@ -4,6 +4,7 @@ import { AlamatGroup } from "../AlamatGroup/AlamatGroup"
 import { JenisKelaminDropDown } from "../JenisKelaminDropDown/JenisKelaminDropDown"
 import { useForm, Controller, SubmitHandler } from "react-hook-form"
 import { IPerson } from "../../features/person/person-slice"
+import { ControlledTextField } from "../ControlledTextField/ControlledTextField"
 
 
 const stackTokens: IStackTokens = { childrenGap: 8 };
@@ -12,67 +13,47 @@ const textFieldStyles: Partial<ITextFieldStyles> = { fieldGroup: { width: 300 } 
 export const PersonFormulir: React.FunctionComponent = () => {
     const { control, handleSubmit } = useForm<IPerson>();
 
-    const onSubmit: SubmitHandler<IPerson> = data => {
-        console.log(data);
+    const onButtonSimpanClick = () => { 
+        handleSubmit(
+            (data) => {
+              console.log(data);
+            },
+            (err) => {                
+              console.log(err);
+            }
+          )();
     };
-
-    const [nikValue, setNikValue] = useState('');
-    const [namaValue, setNamaValue] = useState('');
-    const [tlpValue, setTlpValue] = useState('');
-
-    const onChangeNikValue = useCallback(
-        (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>, newNikValue?: string) => {
-            setNikValue(newNikValue||'');
-        },
-        []
-    );
-
-    const onChangeNamaValue = useCallback(
-        (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>, newNamaValue?: string) => {
-            setNamaValue(newNamaValue||'');
-        },
-        []
-    );
-
-    const onChangeTlpValue = useCallback(
-        (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>, newTlpValue?: string) => {
-            setTlpValue(newTlpValue||'');
-        },
-        []
-    );
-
-    const onButtonSimpanClick = () => { alert('simpan')}
 
     return (
         <div style={{
             display: "inline-block", boxShadow: DefaultEffects.elevation8, 
             borderTop: '2px solid #0078D7', borderRadius: 3, padding: 16, margin: 16}}>
-            <Stack tokens={stackTokens}>                
-                <TextField 
+            <Stack tokens={stackTokens}>  
+                <ControlledTextField
+                    required={true}
                     label="NIK"
-                    placeholder="Isi sesuai dengan ktp"
-                    value={nikValue}
-                    onChange={onChangeNikValue}
-                    styles={textFieldStyles}
-                    required
-                />       
-                <TextField 
+                    control={control}
+                    name={"nik"}
+                    rules={{ required: "harus diisi sesuai dengan ktp" }}                    
+                    styles={textFieldStyles}    
+                />
+                <ControlledTextField
+                    required={true}
                     label="Nama"
-                    placeholder="Isi sesuai dengan ktp"
-                    value={namaValue}
-                    onChange={onChangeNamaValue}
-                    styles={textFieldStyles}
-                    required
+                    control={control}
+                    name={"nama"}
+                    rules={{ required: "harus diisi sesuai dengan ktp" }}                    
+                    styles={textFieldStyles}    
                 /> 
                 <JenisKelaminDropDown />   
-                <TextField 
+                <ControlledTextField
+                    required={true}
                     label="Telepone"
-                    placeholder="Isi dengan nomer Hp / rumah yang aktif"
-                    value={tlpValue}
-                    onChange={onChangeTlpValue}
-                    styles={textFieldStyles}
-                    required
-                />      
+                    control={control}
+                    name={"telepone"}
+                    rules={{ required: "minimal harus diisi satu nomor telepone yang aktif" }}                    
+                    styles={textFieldStyles}    
+                />     
                 <AlamatGroup title="Alamat"/>
                 <PrimaryButton text="Simpan" onClick={onButtonSimpanClick} style={{marginTop: 16, width: 100}}/>
             </Stack>            
