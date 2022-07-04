3070ba64b467b4c8d1ce5d06e5b2a2359711ca04
marksda <vlack123@gmail.com>
2022-07-04 12:07:58 +0700

    cso

---
+++ src/features/person/person-api-slice.tsx
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { baseUrl, defaultKabupaten } from "../config/config";
+import { IPerson } from "./person-slice";
+
+
+export const PersonApiSlice = createApi({
+    reducerPath: 'personApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: baseUrl,
+    }),
+    endpoints(builder) {
+        return {
+            getAllPerson: builder.query<IPerson[], number|void>({
+                query: () => `/person`,
+            }),
+            getPersonByPage: builder.query<IPerson[], number|void>({
+                query: (page = 1, pageSize = 10) => `person/page?page=${page}&pageSize=${pageSize}`,
+            }),
+            getPersonByNama: builder.query<IPerson[], string|void>({
+                query: (nama) => `person/nama?nama=${nama}`,
+            }),
+            getPersonByNamaAndPage: builder.query<IPerson[], string|void>({
+                query: (nama, page=1, pageSize=10) => `person/nama?nama=${nama}&page=${page}&pageSize=${pageSize}`,
+            }),
+            getPersonByPemrakarsa: builder.query<IPerson[], string|void>({
+                query: (idPemrakarsa) => `person/pemrakarsa?idPemrakarsa=${idPemrakarsa}`,
+            }),
+        }
+    }
+})
+
+export const { useGetAllPersonQuery, useGetPersonByPageQuery, useGetPersonByNamaQuery,
+    useGetPersonByNamaAndPageQuery, useGetPersonByPemrakarsaQuery } = PersonApiSlice;
