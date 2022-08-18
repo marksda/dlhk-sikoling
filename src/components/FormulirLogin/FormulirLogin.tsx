612b7f05fd4804ae669b2be5c6805612db6019da
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-08-18 07:27:37 +0700

    cso

--- src/components/FormulirLogin/FormulirLogin.tsx
+++ src/components/FormulirLogin/FormulirLogin.tsx
@@ -1,6 +1,6 @@
-import { ActionButton, DefaultEffects, IIconProps, ILabelStyles, Image, IStackProps, Label, PrimaryButton, Stack, TextField } from "@fluentui/react";
-import { FC, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { ActionButton, DefaultEffects, IconButton, IIconProps, ILabelStyles, Image, Label, PrimaryButton, Stack, TextField } from "@fluentui/react";
+import { FC, useCallback, useState } from "react";
+import { motion } from "framer-motion";
 import { useCekUserNameQuery } from "../../features/security/authorization-api-slice";
 import logo from '../../sidoarjo.svg';
 
@@ -11,14 +11,25 @@ const labelStyle: ILabelStyles  = {
        fontWeight: 600,
        color: '#1b1b1b',
        fontSize: '1.5rem', 
-       marginTop: 16
+    }
+};
+const labelUserNameStyle: ILabelStyles  = {
+    root: {
+       fontWeight: 400,
+       fontSize: '1rem', 
     }
 };
 const contactIcon: IIconProps = { iconName: 'Contact' };
-const lockIcon: IIconProps = { iconName: 'Lock' };
+const backIcon: IIconProps = { 
+    iconName: 'Back',
+    style: {
+        color: 'grey',
+        fontSize: '0.8rem',
+    }
+ };
 const addFriendIcon: IIconProps = { iconName: 'AddFriend' };
 const settingIcon: IIconProps = { iconName: 'PlayerSettings' };
-const duration: number = 1;
+const duration: number = 0.5;
 const variantsUserName = {
     open: { 
         opacity: 1, 
@@ -56,9 +67,23 @@ export const FormulirLogin: FC = () => {
     const [animUserName, setAnimUserName] = useState<string>('open');
     const [animPassword, setAnimPassword] = useState<string>('closed');
     const [flipDisplay, setFlipDisplay] = useState<boolean>(true);
-    // const [userName, setUserName] = useState<string>('');
-    // const { data: isAda = [], isFetching: isFetchingDataCekuserName } = useCekUserNameQuery(userName);
-    // const [ addPerson ] = useAddPersonMutation();
+    const [userName, setUserName] = useState<string>('');
+    const [userPassword, setUserPassword] = useState<string>('');
+    // const { data: dataCekUserName = [], isFetching: isFetchingDataCekuserName } = useCekUserNameQuery(userName);
+
+    const onChangeUserNameValue = useCallback(
+        (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+            setUserName(newValue || '');
+        },
+        [],
+    );
+
+    const onChangeUserPasswordValue = useCallback(
+        (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+            setUserPassword(newValue || '');
+        },
+        [],
+    );
 
     const onButtonLanjutClick = () => {         
         setAnimUserName(animUserName=='open'?'closed':'open'); 
@@ -72,9 +97,21 @@ export const FormulirLogin: FC = () => {
     };
 
     const onButtonMasukClick = () => {         
+        // setAnimPassword(animPassword=='open'?'closed':'open');
+        // setTimeout(
+        //     () => {
+        //         setFlipDisplay(!flipDisplay);
+        //         setAnimUserName(animUserName=='open'?'closed':'open'); 
+        //     },
+        //     duration*1000
+        // );
+    };
+
+    const onButtonUserNameBackClick = () => {         
         setAnimPassword(animPassword=='open'?'closed':'open');
         setTimeout(
             () => {
+                setUserPassword('');
                 setFlipDisplay(!flipDisplay);
                 setAnimUserName(animUserName=='open'?'closed':'open'); 
             },
@@ -91,13 +128,22 @@ export const FormulirLogin: FC = () => {
                 height={42}
                 src={logo}
             />
+            <div style={{height: 8}}></div>
             <motion.div
                 animate={animUserName}
                 variants={variantsUserName}
                 style={flipDisplay?{display:'block'}:{display:'none'}}
             >
-                <Label styles={labelStyle}>Sign in</Label>
-                <TextField placeholder="user name" iconProps={contactIcon} underlined styles={{root: {marginBottom: 8, width: 300}}}/>
+                <Stack horizontal tokens={stackTokens} styles={{root: { width: 300, alignItems: 'center'}}}>
+                    <Label styles={labelStyle}>Masuk</Label>
+                </Stack>
+                <TextField 
+                    placeholder="Email, telepon, atau nik" 
+                    value={userName}
+                    onChange={onChangeUserNameValue}
+                    iconProps={contactIcon} 
+                    underlined 
+                    styles={{root: {marginBottom: 8, width: 300}}}/>
                 <Stack horizontal tokens={stackTokens} styles={{root: { width: 300, alignItems: 'center'}}}>
                     <Label styles={{root: {fontWeight: 500, color: '#656363'}}}>Belum punya akun?</Label> 
                     <ActionButton iconProps={addFriendIcon} styles={{root: {color: '#0067b8'}}}>
@@ -117,8 +163,34 @@ export const FormulirLogin: FC = () => {
                 variants={variantsPassword}
                 style={!flipDisplay?{display:'block'}:{display:'none'}}
             >
-                <Label styles={labelStyle}>nama</Label>
-                <TextField placeholder="user name" iconProps={lockIcon} underlined styles={{root: {marginBottom: 8, width: 300}}}/>
+                <Stack horizontal tokens={stackTokens} styles={{root: { width: 300, alignItems: 'center'}}}>                    
+                    <IconButton 
+                        iconProps={backIcon} 
+                        title="Back" 
+                        ariaLabel="Back"
+                        onClick={onButtonUserNameBackClick} 
+                        styles={{
+                            root: {
+                                borderStyle: 'none',
+                                borderRadius: '50%',
+                                padding: 0,
+                                marginTop: 2,
+                            }
+                        }}/>
+                    <Label styles={labelUserNameStyle}>{userName}</Label>
+                </Stack>                
+                <Stack horizontal tokens={stackTokens} styles={{root: { width: 300, alignItems: 'center'}}}>
+                    <Label styles={labelStyle}>Masukkan sandi</Label>
+                </Stack>                
+                <TextField 
+                    placeholder="Sandi" 
+                    value={userPassword}
+                    onChange={onChangeUserPasswordValue}
+                    underlined 
+                    type="password"
+                    canRevealPassword
+                    revealPasswordAriaLabel="Tunjukkan sandi"
+                    styles={{root: {marginBottom: 8, width: 300}}}/>
                 <Stack horizontal tokens={stackTokens} styles={{root: { width: 300, alignItems: 'center'}}}>
                     <Label styles={{root: {fontWeight: 500, color: '#656363'}}}>Lupa password?</Label> 
                     <ActionButton iconProps={settingIcon} styles={{root: {color: '#0067b8'}}}>
