9f129b7ba11bd0798e26f86129fbe842152621b2
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-08-31 14:58:02 +0700

    cso

---
+++ src/page/header.tsx
@@ -0,0 +1,42 @@
+import { DefaultPalette, Image, IStackItemStyles, IStackStyles, IStackTokens, Stack } from "@fluentui/react";
+import { FC } from "react";
+import logo from '../sidoarjo.svg';
+
+const headerStackTokens: IStackTokens = { childrenGap: 16};
+const stackStyles: IStackStyles = {
+    root: {
+      background: '#3c8dbc',
+      paddingLeft: 52,
+      paddingRight: 52,
+    },
+};
+const labelStyles: IStackItemStyles = {
+    root: {
+      color: DefaultPalette.white,
+      fontSize: '1.4em',
+      fontWeight: 500
+    },
+};
+const dateStyles: IStackItemStyles = {
+    root: {
+      color: DefaultPalette.white,
+      fontSize: '1.0em',
+    },
+};
+
+export const Header: FC = () => {
+    const today = new Date().toLocaleDateString();
+    return (
+        <Stack horizontal styles={stackStyles} tokens={headerStackTokens}>
+            <Stack.Item>
+                <Image alt='logo' width={56} height={56} src={logo}/>
+            </Stack.Item>
+            <Stack.Item grow align="center" styles={labelStyles}>
+                DLHK Sidoarjo    
+            </Stack.Item>   
+            <Stack.Item align="center" styles={dateStyles}>
+                {`${today}`}
+            </Stack.Item>             
+        </Stack>
+    );
+}
--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,32 +1,11 @@
 import { DefaultPalette, Image, IStackItemStyles, IStackStyles, IStackTokens, Stack } from "@fluentui/react";
 import { FC } from "react";
 import { FormulirLogin } from "../components/FormulirLogin/FormulirLogin";
-import logo from '../sidoarjo.svg';
+import { Header } from "./header";
 
 
 const containerStackTokens: IStackTokens = { childrenGap: 5};
-const headerStackTokens: IStackTokens = { childrenGap: 16};
 const containerBodyStackTokens: IStackTokens = { childrenGap: 5};
-const stackStyles: IStackStyles = {
-    root: {
-      background: '#3c8dbc',
-      paddingLeft: 52,
-      paddingRight: 52,
-    },
-};
-const labelStyles: IStackItemStyles = {
-    root: {
-      color: DefaultPalette.white,
-      fontSize: '1.4em',
-      fontWeight: 500
-    },
-};
-const dateStyles: IStackItemStyles = {
-    root: {
-      color: DefaultPalette.white,
-      fontSize: '1.0em',
-    },
-};
 const stackBodyStyles: IStackStyles = {
     root: {
       paddingLeft: 16,
@@ -38,17 +17,7 @@ export const Home: FC = () => {
     const today = new Date().toLocaleDateString();
     return (
         <Stack tokens={containerStackTokens} >
-            <Stack horizontal styles={stackStyles} tokens={headerStackTokens}>
-                <Stack.Item>
-                    <Image alt='logo' width={56} height={56} src={logo}/>
-                </Stack.Item>
-                <Stack.Item grow align="center" styles={labelStyles}>
-                    DLHK Sidoarjo    
-                </Stack.Item>   
-                <Stack.Item align="center" styles={dateStyles}>
-                    {`${today}`}
-                </Stack.Item>             
-            </Stack>   
+            <Header />
             <Stack horizontal reversed tokens={containerBodyStackTokens} styles={stackBodyStyles}>
                 <FormulirLogin />
             </Stack>             
