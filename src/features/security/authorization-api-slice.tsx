607eaf68c9061356fc324e2237eefa64455135fc
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-09-15 08:26:11 +0700

    cso

--- src/features/security/authorization-api-slice.tsx
+++ src/features/security/authorization-api-slice.tsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { baseRestAPIUrl } from "../../features/config/config";
 import { IPerson } from "../person/person-slice";
-import { IAuthentication } from "./authorization-slice";
+import { IAuthentication } from "./authentication-slice";
 import { IResponseStatusToken } from "./token-slice";
 
 export interface PostRegistrasi {
