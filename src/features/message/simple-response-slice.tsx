8a2c5834833e4a85ac9ad6fecd1f4789ef0e7a93
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-09-23 11:14:19 +0700

    cso

---
+++ src/features/message/simple-response-slice.tsx
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface ISimpleResponse {
+    status: string;
+    pesan: string;
+}
+
+const initialState: ISimpleResponse ={
+    status: '',
+    pesan: '',
+};
+
+//redux busines logic
+export const SimpleResponseSlice = createSlice({
+    name: 'propinsi',
+    initialState,
+    reducers: {
+        setSimpleResponse: (state, action: PayloadAction<ISimpleResponse>) => {
+            state.status = action.payload.status;
+            state.pesan = action.payload.pesan;
+        },
+        setStatus: (state, action: PayloadAction<string>) => {
+            state.status = action.payload;
+        },
+        setPesan: (state, action: PayloadAction<string>) => {
+            state.pesan = action.payload;
+        },
+    },
+}); 
+
+// redux action creator
+export const { setSimpleResponse, setStatus, setPesan } = SimpleResponseSlice.actions;
+
+export default SimpleResponseSlice.reducer;
