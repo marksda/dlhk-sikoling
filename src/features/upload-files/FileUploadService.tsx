0079cc0af4f954f57e39c5673e88fff6003480a2
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-09-26 20:46:29 +0700

    cso

--- src/features/upload-files/FileUploadService.tsx
+++ src/features/upload-files/FileUploadService.tsx
@@ -3,13 +3,13 @@ import http from "./http-common"
 const upload = (files: any, onUploadProgress: any) => {
     let formData = new FormData();
     let i: number = 1;
-    
+
     for(let dataFile of files) {
-        formData.append(`file${i}`, dataFile);
+        formData.append(`file-${i}`, dataFile);
         i++;
     }    
     
-    return http.post("/files/nosc", formData, {
+    return http.post("/files/nosec", formData, {
         headers: {
             "Content-Type": "multipart/form-data",
         },
