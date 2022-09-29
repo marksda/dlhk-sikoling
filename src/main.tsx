dc50960c1d04bdcf9d6d69ab432d0535ab5b3726
marksda <vlack123@gmail.com>
2022-09-29 12:17:10 +0700

    cso

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { initializeIcons } from '@fluentui/font-icons-mdl2';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import './index.css';
-// import RoutePage from './RoutePage';
 import { Home } from './page/home';
 import { AdminPage } from './page/main-admin';
 import { UserRegistrasi } from './page/user-registrasi';
@@ -14,17 +12,17 @@ import { NotifikasiRegistrasi } from './page/notif-registrasi';
 initializeIcons()
 
 const container = document.getElementById('root');
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+const root = createRoot(container!);
 
 
 root.render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}/>   
-          <Route path="admin" element={<AdminPage/>}/>      
-          <Route path="registrasi" element={<UserRegistrasi/>}/> 
-          <Route path="notif_registrasi"element={<NotifikasiRegistrasi/>}/> 
+          <Route path="/" element={<AdminPage />}/>   
+          <Route path="admin" element={<Home />}/>      
+          <Route path="registrasi" element={<UserRegistrasi />}/> 
+          <Route path="notif_registrasi"element={<NotifikasiRegistrasi />}/> 
         </Routes>
       </BrowserRouter>
     </Provider>    
--- src/page/main-admin.tsx
+++ src/page/main-admin.tsx
@@ -1,24 +1,56 @@
-import { getTheme, IStackTokens, Stack } from "@fluentui/react";
+import { getTheme, IStackItemStyles, IStackStyles, IStackTokens, Stack } from "@fluentui/react";
 import { FC } from "react";
 
-// const containerStyle: React.CSSProperties = {
-//     width: '100vw',
-//     height: '100vh',
-//     background: 'RGB(204, 203, 202)'
-// };
-
-const containerStackTokens: IStackTokens = { childrenGap: 5};
 const theme = getTheme();
-const containerStyles: React.CSSProperties = {
-    backgroundColor: theme.palette.themePrimary,
-    color: theme.palette.white,
-    lineHeight: '50px',
-    padding: '0 20px',
+const stackStyles: IStackStyles = {
+    root: {
+        backgroundColor: theme.palette.themePrimary,
+        color: theme.palette.white,
+        lineHeight: '50px',
+        padding: '0 20px',
+    },
 };
+const stackMainContainerStyles: IStackStyles = {
+    root: {
+        backgroundColor: '#E1E3E4'
+    },
+};
+const labelStyles: IStackItemStyles = {
+    root: {
+      fontSize: '1.2em',
+      fontWeight: 500
+    },
+};
+const leftPanelStyles: IStackItemStyles = {
+    root: {
+        height: 'calc(100vh - 50px)',
+        width: '300px',
+    },
+};
+
 export const AdminPage: FC = () => {
     return (        
-        <Stack tokens={containerStackTokens} >
-            <div style={containerStyles}>Hello world</div>
+        <Stack>
+            <Stack horizontal styles={stackStyles}>
+                <Stack.Item grow align="center" styles={labelStyles}>
+                    Sikoling  
+                </Stack.Item>  
+            </Stack>
+            <Stack horizontal styles={stackMainContainerStyles}>
+                <Stack.Item styles={leftPanelStyles}>
+                    Left menu Sikoling  
+                </Stack.Item>  
+                <Stack.Item>
+                    <Stack>
+                        <Stack.Item>
+                            Breadcum
+                        </Stack.Item>
+                        <Stack.Item>
+                            Isi utama
+                        </Stack.Item>
+                    </Stack>
+                </Stack.Item>
+            </Stack>
         </Stack>
     );
 }
