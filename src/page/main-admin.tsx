f06f165831f1d34965133d1d4de9bff9de9c5162
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-10-01 00:22:38 +0700

    cso

--- src/page/main-admin.tsx
+++ src/page/main-admin.tsx
@@ -1,5 +1,6 @@
 import { getTheme, INavLinkGroup, INavStyles, IStackItemStyles, IStackStyles, IStackTokens, Nav, Stack } from "@fluentui/react";
 import { FC } from "react";
+import { LeftMenuPage } from "./template-left-menu";
 
 const theme = getTheme();
 const stackStyles: IStackStyles = {
@@ -23,151 +24,20 @@ const labelStyles: IStackItemStyles = {
 };
 const leftPanelStyles: IStackItemStyles = {
     root: {
-        height: 'calc(100vh - 50px)',
-        width: '300px',
+        height: 'calc(100vh - 68px)',
+        width: 300,
+        padding: 8,        
+        border: '1px solid #eee',
     },
 };
-const navStyles: Partial<INavStyles> = {
+const rightPanelStyles: IStackItemStyles = {
     root: {
-      width: 285,
-      height: 'calc(100vh - 50px)',
-      boxSizing: 'border-box',
-      border: '1px solid #eee',
-      overflowY: 'auto',
+        height: 'calc(100vh - 68px)',
+        padding: 8,        
     },
 };
-const navLinkGroups: INavLinkGroup[] = [
-    {
-      links: [
-        {
-          name: 'Helpdesk',
-          url: 'http://example.com',
-          expandAriaLabel: 'Expand Helpdesk section',
-          links: [
-            {
-                name: 'Pendaftaran',
-                url: 'http://msn.com',
-                key: '00',
-                target: '_blank',
-            },
-            {
-                name: 'Pendaftaran Online',
-                url: 'http://msn.com',
-                disabled: true,
-                key: '01',
-                target: '_blank',
-            },
-            {
-                name: 'Pendaftaran Online',
-                url: 'http://msn.com',
-                disabled: true,
-                key: '02',
-                target: '_blank',
-            },
-            {
-                name: 'Data dalam proses',
-                url: 'http://msn.com',
-                disabled: true,
-                key: '03',
-                target: '_blank',
-            },
-            {
-                name: 'Data Ditolak',
-                url: 'http://msn.com',
-                disabled: true,
-                key: '04',
-                target: '_blank',
-            },
-          ],
-          isExpanded: true,
-        },
-        {
-            name: 'Back Office',
-            url: 'http://example.com',
-            expandAriaLabel: 'Expand Back Office section',
-            links: [
-              {
-                  name: 'Data Ditolak',
-                  url: 'http://msn.com',
-                  key: '10',
-                  target: '_blank',
-              },
-              {
-                  name: 'Data Dari Helpdesk',
-                  url: 'http://msn.com',
-                  disabled: true,
-                  key: '11',
-                  target: '_blank',
-              },
-              {
-                  name: 'Korektor',
-                  url: 'http://msn.com',
-                  disabled: true,
-                  key: '12',
-                  target: '_blank',
-              },
-              {
-                  name: 'Edit/Pencetakan Draf',
-                  url: 'http://msn.com',
-                  disabled: true,
-                  key: '13',
-                  target: '_blank',
-              },
-              {
-                  name: 'Kasi Verifikasi',
-                  url: 'http://msn.com',
-                  disabled: true,
-                  key: '14',
-                  target: '_blank',
-              },
-              {
-                name: 'Kabid Verifikasi',
-                url: 'http://msn.com',
-                disabled: true,
-                key: '15',
-                target: '_blank',
-            },
-            ],
-            isExpanded: false,
-        },
-        {
-          name: 'Documents',
-          url: 'http://example.com',
-          key: 'key3',
-          isExpanded: true,
-          target: '_blank',
-        },
-        {
-          name: 'Pages',
-          url: 'http://msn.com',
-          key: 'key4',
-          target: '_blank',
-        },
-        {
-          name: 'Notebook',
-          url: 'http://msn.com',
-          key: 'key5',
-          disabled: true,
-        },
-        {
-          name: 'Communication and Media',
-          url: 'http://msn.com',
-          key: 'key6',
-          target: '_blank',
-        },
-        {
-          name: 'News',
-          url: 'http://cnn.com',
-          icon: 'News',
-          key: 'key7',
-          target: '_blank',
-        },
-      ],
-    },
-  ];
 
 export const AdminPage: FC = () => {
-
     return (        
         <Stack>
             <Stack horizontal styles={stackStyles}>
@@ -177,14 +47,9 @@ export const AdminPage: FC = () => {
             </Stack>
             <Stack horizontal styles={stackMainContainerStyles}>
                 <Stack.Item styles={leftPanelStyles}>
-                <Nav
-                    selectedKey="00"
-                    ariaLabel="left menu sikoling"
-                    styles={navStyles}
-                    groups={navLinkGroups}
-                />  
+                    <LeftMenuPage />
                 </Stack.Item>  
-                <Stack.Item>
+                <Stack.Item styles={rightPanelStyles}>
                     <Stack>
                         <Stack.Item>
                             Breadcum
---
+++ src/page/template-left-menu.tsx
@@ -0,0 +1,49 @@
+import { INavLinkGroup, INavStyles, Nav } from "@fluentui/react";
+import { FC } from "react";
+
+const navStyles: Partial<INavStyles> = {
+    root: {
+    //   width: 296,
+      height: 'calc(100vh - 68px)',
+      boxSizing: 'border-box',
+      overflowY: 'auto',
+    },
+};
+const navLinkGroups: INavLinkGroup[] = [
+    {
+      links: [
+        {
+          name: 'Permohonan',
+          url: 'http://example.com',
+          icon: 'ChangeEntitlements',
+          key: 'pmh',
+          isExpanded: true,
+          target: '_blank',
+        },
+        {
+          name: 'Pelaporan',
+          url: 'http://msn.com',
+          icon: 'ReportDocument',
+          key: 'plp',
+          target: '_blank',
+        },
+        {
+          name: 'Pengawasan - SKPL',
+          url: 'http://cnn.com',
+          icon: 'ComplianceAudit',
+          key: 'key7',
+          target: '_blank',
+        },
+      ],
+    },
+  ];
+export const LeftMenuPage: FC = () => {
+    return (
+        <Nav
+            selectedKey="pmh"
+            ariaLabel="left menu sikoling"
+            styles={navStyles}
+            groups={navLinkGroups}
+        />  
+    );
+}
