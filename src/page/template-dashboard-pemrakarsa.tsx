e8773bd4424ad444386f7c1b3d88ae1e6af9c436
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-10-04 19:40:14 +0700

    cso

--- src/page/template-dashboard-pemrakarsa.tsx
+++ src/page/template-dashboard-pemrakarsa.tsx
@@ -1,7 +1,8 @@
-import { Breadcrumb, DefaultEffects, IBreadcrumbItem, IStackStyles, Stack } from "@fluentui/react";
+import { Breadcrumb, DefaultEffects, DocumentCard, DocumentCardLogo, DocumentCardType, IBreadcrumbItem, IDocumentCardLogoProps, IDocumentCardStyles, IStackStyles, IStackTokens, Stack } from "@fluentui/react";
 import { FC, useState } from "react";
 import { ListDetailMessage } from "./dashboard/template-list-message";
 
+const stackTokens: IStackTokens = { childrenGap: 1 };
 const kontenStyles: IStackStyles = {
     root: {
         padding: '0px 16px',        
@@ -16,6 +17,15 @@ const containerDivStyles: React.CSSProperties = {
     background: 'white',
     height: 'calc(100vh - 148px)',
     marginLeft: 4,
+};
+const cardStyles: IDocumentCardStyles = {
+    root: { display: 'inline-block', marginRight: 20, width: 120 },
+};
+const logoProps: IDocumentCardLogoProps = {
+    logoIcon: 'OutlookLogo',
+};
+const logoPerusahaanProps: IDocumentCardLogoProps = {
+    logoIcon: 'CityNext',
 };
 
 export const KontenDashboardPemrakarsa: FC = () => {
@@ -37,7 +47,25 @@ export const KontenDashboardPemrakarsa: FC = () => {
             </Stack.Item>
             <Stack.Item grow align="auto">   
                 <div style={containerDivStyles}>
-                    <Stack>
+                    <Stack tokens={stackTokens}>
+                        <Stack.Item>
+                            <Stack horizontal tokens={stackTokens}>
+                                <DocumentCard
+                                    aria-label="Perusahaan"
+                                    styles={cardStyles}
+                                    onClickHref="http://bing.com"
+                                >
+                                    <DocumentCardLogo {...logoPerusahaanProps} />
+                                </DocumentCard>
+                                <DocumentCard
+                                    aria-label="Permohonan"
+                                    styles={cardStyles}
+                                    onClickHref="http://bing.com"
+                                >
+                                    <DocumentCardLogo {...logoProps} />
+                                </DocumentCard>
+                            </Stack>
+                        </Stack.Item>
                         <Stack.Item>
                             <ListDetailMessage />
                         </Stack.Item>
