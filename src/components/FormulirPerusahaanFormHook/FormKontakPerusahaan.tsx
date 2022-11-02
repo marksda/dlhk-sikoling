44cb562deaeaca6b8c41ee1fd934096db7e72d8a
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-11-02 09:26:20 +0700

    cso

--- src/components/FormulirPerusahaanFormHook/FormKontakPerusahaan.tsx
+++ src/components/FormulirPerusahaanFormHook/FormKontakPerusahaan.tsx
@@ -1,7 +1,8 @@
-import { IconButton, Label, Stack } from "@fluentui/react";
+import { IconButton, Label, PrimaryButton, Stack } from "@fluentui/react";
 import { motion } from "framer-motion";
 import { FC, useCallback, useState } from "react";
 import { useWatch } from "react-hook-form";
+import { ControlledFluentUiTextField } from "../ControlledTextField/ControlledFluentUiTextField";
 import { backIcon, contentStyles, duration, ISubFormPerusahaanProps, labelStyle, labelTitleBack, stackTokens, subLabelStyle, variantAnimPerusahaan } from "./InterfacesPerusahaan";
 
 export const FormKontakPerusahaan: FC<ISubFormPerusahaanProps> = ({control, setValue, setMotionKey}) => {
@@ -16,6 +17,20 @@ export const FormKontakPerusahaan: FC<ISubFormPerusahaanProps> = ({control, setV
     const processBackToPreviousStep = useCallback(
         () => {
             setAnimKontakPerusahaan('closed');            
+            let timer = setTimeout(
+                () => {
+                    setMotionKey('alamatPerusahaan');
+                },
+                duration*1000
+            );
+            return () => clearTimeout(timer);
+        },
+        []
+    );
+
+    const processNextStep = useCallback(
+        () => {
+            setAnimKontakPerusahaan('closed');
             let timer = setTimeout(
                 () => {
                     setMotionKey('alamatPerusahaan');
@@ -53,7 +68,43 @@ export const FormKontakPerusahaan: FC<ISubFormPerusahaanProps> = ({control, setV
                 <Label styles={labelStyle}>Kontak Perusahaan</Label>
                 <Label styles={subLabelStyle}>Lengkapi kontak perusahaan sesuai dengan dokumen legalitas pendirian.</Label>
             </Stack>
-            
+            <Stack tokens={stackTokens} styles={{root: { width: 400, alignItems: 'left'}}}>
+                <Stack.Item>
+                    <ControlledFluentUiTextField
+                        label="Email"
+                        name="kontak.email"
+                        rules={{ required: "email perusahaan harus diisi" }} 
+                        required
+                        control={control}
+                    />
+                </Stack.Item>
+                <Stack.Item>
+                    <ControlledFluentUiTextField
+                        label="Telepone"
+                        name="kontak.telepone"
+                        rules={{ required: "Telepone perusahaan harus diisi" }} 
+                        required
+                        control={control}
+                        disabled={kontak.email == '' ? true:false}
+                    />
+                </Stack.Item>
+                <Stack.Item>
+                    <ControlledFluentUiTextField
+                        label="Fax"
+                        name="kontak.fax"
+                        control={control}
+                        disabled={kontak.telepone == '' ? true:false}
+                    />
+                </Stack.Item>
+            </Stack>
+            <Stack horizontal tokens={stackTokens} styles={{root: { width: 400, justifyContent: 'flex-end'}}}>
+                <PrimaryButton 
+                    text="Lanjut" 
+                    style={{marginTop: 24, width: 100}}
+                    onClick={processNextStep}
+                    disabled={kontak.telepone == '' ? true:false}
+                />
+            </Stack>
         </motion.div>
     );
 };
