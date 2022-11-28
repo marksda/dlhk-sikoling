7c451371342781271e816443d7815ab43d3c6f41
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-11-28 13:43:48 +0700

    cso

--- src/entity/KatalogPermohonan.ts
+++ src/entity/KatalogPermohonan.ts
@@ -1,10 +1,24 @@
+import find from "lodash.find";
 import { DeskripsiPermohonan } from "./DeskripsiPermohonan";
 
 export class KatalogPermohonan {
     private _daftarKatalogPermohonan: DeskripsiPermohonan[];
-
+    
     constructor(daftarKatalogPermohonan: DeskripsiPermohonan[]) {
         this._daftarKatalogPermohonan = daftarKatalogPermohonan;
     }
 
+    public get daftarKatalogPermohonan(): DeskripsiPermohonan[] {
+        return this._daftarKatalogPermohonan;
+    }
+
+    public set daftarKatalogPermohonan(value: DeskripsiPermohonan[]) {
+        this._daftarKatalogPermohonan = value;
+    }
+
+    public getDeskripsiPermohonan(itemId: string): DeskripsiPermohonan|undefined {
+        return find(this._daftarKatalogPermohonan , (obj) => {
+            return obj.id == itemId;
+        });
+    }
 }
