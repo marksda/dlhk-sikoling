9ca165743b31f72951bf1a2cf60b57d80ff45e7c
Syaiful Dwi Anwar <vlack123@gmail.com>
2022-12-14 12:57:53 +0700

    cso

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,5 +1,5 @@
 import { IStackStyles, IStackTokens, Stack } from "@fluentui/react";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../app/hooks";
 // import { useAppDispatch, useAppSelector } from "../app/hooks";
@@ -17,70 +17,33 @@ const stackBodyStyles: IStackStyles = {
     },
 };
 
-export const Home: FC = () => {
+export const Home = () => {
     const token = useAppSelector((state) => state.token);
-    //react redux
-    // const dispatch = useAppDispatch();
-    //react router
     const navigate = useNavigate();
 
-    // if(token.hakAkses == null) {
-    //     // return (
-            // <Stack tokens={containerStackTokens} >
-            //     <Header />
-            //     <Stack horizontal reversed tokens={containerBodyStackTokens} styles={stackBodyStyles}>
-            //         <FormulirLogin />
-            //     </Stack>             
-            // </Stack>
-    //     // )
-    // }
-    // else {
-    //     switch (token.hakAkses) {
-            // case 'Umum':
-            //     navigate("/pemrakarsa");
-            //     break;   
-            // case 'admin':
-            //     navigate("/admin");
-            //     break;                
-            // default:
-            //     break;
-    //     }
-    //     // return null;
-    // }
-
-    // useEffect(
-    //     () => {            
-    //         if(token.hakAkses == null) {
-    //             let tokenString = localStorage.getItem('token');
-    //             if(tokenString != null){
-    //                 dispatch(setToken(JSON.parse(tokenString)));
-    //             }                
-    //         }
-    //         else {
-    //             switch (token.hakAkses) {
-    //                 case 'Umum':
-    //                     navigate("/pemrakarsa");
-    //                     break;   
-    //                 case 'admin':
-    //                     navigate("/admin");
-    //                     break;                
-    //                 default:
-    //                     break;
-    //             }
-    //         }            
-    //     },
-    //     [token]
-    // );
-
-    return (
-        {
-            token.hakAkses == null &&
-            <Stack tokens={containerStackTokens} >
-                <Header />
-                <Stack horizontal reversed tokens={containerBodyStackTokens} styles={stackBodyStyles}>
-                    <FormulirLogin />
-                </Stack>             
-            </Stack> 
-        }
+    useEffect(
+        () => {
+            switch (token.hakAkses) {
+                case 'Umum':
+                    navigate("/pemrakarsa");
+                    break;
+                case 'admin':
+                    navigate("/admin");
+                    break;
+                default:
+                    break;
+            }
+        },
+        [token]
     );
+        
+    return (
+        <Stack tokens={containerStackTokens} >
+            <Header />
+            <Stack horizontal reversed tokens={containerBodyStackTokens} styles={stackBodyStyles}>
+                <FormulirLogin />
+            </Stack>             
+        </Stack>
+    );
+    
 }
