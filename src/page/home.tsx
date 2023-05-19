1ae7d6102e1c614d652d9981173847b46728aa6e
Syaiful Dwi Anwar <vlack123@gmail.com>
2023-05-19 10:02:04 +0700

    cso

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../app/hooks";
 import { FormulirLogin } from "../components/FormulirLogin/FormulirLogin";
 import { Header } from "./header";
+import { ErrorPage } from "../error-page";
 
 const containerStackTokens: IStackTokens = { childrenGap: 5};
 const containerBodyStackTokens: IStackTokens = { childrenGap: 5};
@@ -18,34 +19,34 @@ export const Home = () => {
     const token = useAppSelector((state) => state.token);
     const navigate = useNavigate();
 
-    useEffect(
-        () => {
-            switch (token.hakAkses) {
-                case 'Umum':
-                    navigate("/pemrakarsa");
-                    break;
-                case 'Administrator':
-                    navigate("/admin");
-                    break;
-                default:
-                    break;
-            }
-        },
-        [token]
-    );
+    // useEffect(
+    //     () => {
+    //         switch (token.hakAkses) {
+    //             case 'Umum':
+    //                 navigate("/pemrakarsa");
+    //                 break;
+    //             case 'Administrator':
+    //                 navigate("/admin");
+    //                 break;
+    //             default:
+    //                 break;
+    //         }
+    //     },
+    //     [token]
+    // );
         
-    return (
-        <>
-        {
-            token.hakAkses == null ? (
-            <Stack tokens={containerStackTokens} >
-                <Header />
-                <Stack horizontal reversed tokens={containerBodyStackTokens} styles={stackBodyStyles}>
-                    <FormulirLogin />
-                </Stack>             
-            </Stack>) : null
-        }
-        </>
-    );
+    return (<>{
+        token.hakAkses == null ? 
+        <Stack tokens={containerStackTokens} >
+            <Header />
+            <Stack horizontal reversed tokens={containerBodyStackTokens} styles={stackBodyStyles}>
+                <FormulirLogin />
+            </Stack>             
+        </Stack> : token.hakAkses == 'Administrator' ?
+        navigate("/admin") : token.hakAkses == 'umum' ?
+        navigate("/pemrakarsa") : <ErrorPage />
+    }</>);
+        
+    
     
 }
