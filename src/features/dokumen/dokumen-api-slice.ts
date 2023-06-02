88d1702675854a2be50e5fd6f8f17536cc034a44
Syaiful Dwi Anwar <vlack123@gmail.com>
2023-06-02 08:43:22 +0700

    cso

--- src/features/dokumen/dokumen-api-slice.ts
+++ src/features/dokumen/dokumen-api-slice.ts
@@ -2,6 +2,7 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { baseQueryWithReauth } from "../config/helper-function";
 import { IDokumenNibOss } from "./dokumen-nib-oss-slice";
 import { IDokumen } from "./dokumen-slice";
+import { IQueryParams } from "../config/query-params-slice";
 
 type daftarDokumen = IDokumen[];
 
@@ -9,8 +10,7 @@ export const DokumenApiSlice = createApi({
     reducerPath: 'dokumenApi',
     baseQuery: baseQueryWithReauth,
     refetchOnReconnect: true,
-    keepUnusedDataFor: 30,
-    tagTypes:['Dokumen', 'DokumenPage', 'DokumenNama', 'DokumenNamaPage'],
+    tagTypes:['Dokumen'],
     endpoints(builder) {
         return {            
             addDokumen: builder.mutation<IDokumen, Partial<IDokumen>|Partial<IDokumenNibOss>>({
@@ -19,7 +19,7 @@ export const DokumenApiSlice = createApi({
                     method: 'POST',
                     body,
                 }),
-                invalidatesTags: [{type: 'Dokumen', id: 'LIST'}, {type: 'DokumenPage', id: 'LIST'}, {type: 'DokumenNama', id: 'LIST'}, {type: 'DokumenNamaPage', id: 'LIST'}],
+                invalidatesTags: [{type: 'Dokumen', id: 'LIST'}],
             }),
             updateDokumen: builder.mutation<void, {id: string; dokumen: IDokumen}>({
                 query: ({id, dokumen}) => ({
@@ -27,19 +27,29 @@ export const DokumenApiSlice = createApi({
                     method: 'PUT',
                     body: dokumen,
                 }),
-                invalidatesTags: (result, error, { id }) => [{type: 'Dokumen', id}, {type: 'DokumenPage', id}, {type: 'DokumenNama', id}, {type: 'DokumenNamaPage', id}],
+                invalidatesTags: (result, error, { id }) => [{type: 'Dokumen', id}],
             }),
-            deleteKategoriDokumen: builder.mutation<{ success: boolean; id: string }, string>({
+            updateDokunentById: builder.mutation<void, {id: string; dokumen: IDokumen}>({
+                query: ({id, dokumen}) => ({
+                    url: `dokumen/id/${id}`,
+                    method: 'PUT',
+                    body: dokumen,
+                }),
+                invalidatesTags: (result, error, { id }) => {
+                    return [{type: 'Dokumen', id}];
+                }
+            }),
+            deleteDokumen: builder.mutation<{ success: boolean; id: string }, string>({
                 query(id) {
                   return {
                     url: `dokumen/${id}`,
                     method: 'DELETE',
                   }
                 },
-                invalidatesTags: (result, error, id) => [{ type: 'Dokumen', id }, { type: 'DokumenPage', id }, { type: 'DokumenNama', id }, { type: 'DokumenNamaPage', id }],
+                invalidatesTags: (result, error, id) => [{ type: 'Dokumen', id }],
             }),
-            getAllDokumen: builder.query<daftarDokumen, void>({
-                query: () => `dokumen`,
+            getDaftarDokumentByFilter: builder.query<daftarDokumen, IQueryParams>({
+                query: (queryParams) => `dokumen?filters=${JSON.stringify(queryParams)}`,
                 providesTags: (result) => 
                     result ?
                     [
@@ -50,41 +60,8 @@ export const DokumenApiSlice = createApi({
                     ]:
                     [{type: 'Dokumen', id: 'LIST'}],
             }),
-            getDokumenByPage: builder.query<daftarDokumen, {page: number; pageSize: number}>({
-                query: ({page, pageSize}) => `dokumen/page?page=${page}&pageSize=${pageSize}`,
-                providesTags: (result) => 
-                    result ?
-                    [
-                        ...result.map(
-                            ({ id }) => ({ type: 'DokumenPage' as const, id: id! })
-                        ),
-                        { type: 'DokumenPage', id: 'LIST' },
-                    ]:
-                    [{type: 'DokumenPage', id: 'LIST'}],
-            }),
-            getDokumenByNama: builder.query<daftarDokumen, string>({
-                query: (nama) => `dokumen/nama?nama=${nama}`,
-                providesTags: (result) => 
-                    result ?
-                    [
-                        ...result.map(
-                            ({ id }) => ({ type: 'DokumenNama' as const, id: id! })
-                        ),
-                        { type: 'DokumenNama', id: 'LIST' },
-                    ]:
-                    [{type: 'DokumenNama', id: 'LIST'}],
-            }),
-            getDokumenByNamaAndPage: builder.query<daftarDokumen, {nama: string; page: number; pageSize: number}>({
-                query: ({nama, page, pageSize}) => `dokumen/nama?nama=${nama}&page=${page}&pageSize=${pageSize}`,
-                providesTags: (result) => 
-                    result ?
-                    [
-                        ...result.map(
-                            ({ id }) => ({ type: 'DokumenNamaPage' as const, id: id! })
-                        ),
-                        { type: 'DokumenNamaPage', id: 'LIST' },
-                    ]:
-                    [{type: 'DokumenNamaPage', id: 'LIST'}],
+            getTotalCountDokumen: builder.query<number, Pick<IQueryParams, "filters">>({
+                query: (queryFilters) => `dokumen/count?filters=${JSON.stringify(queryFilters)}`,
             }),
         };
     }
@@ -92,7 +69,6 @@ export const DokumenApiSlice = createApi({
 
 export const {
     useAddDokumenMutation, useUpdateDokumenMutation,
-    useDeleteKategoriDokumenMutation, useGetAllDokumenQuery,
-    useGetDokumenByPageQuery, useGetDokumenByNamaQuery,
-    useGetDokumenByNamaAndPageQuery
+    useUpdateDokunentByIdMutation, useDeleteDokumenMutation,
+    useGetDaftarDokumentByFilterQuery, useGetTotalCountDokumenQuery
 } = DokumenApiSlice;
