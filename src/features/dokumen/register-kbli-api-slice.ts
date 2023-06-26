0f588f303ca41da57934fcfecd9787c3f78d8a3c
marksda <vlack123@gmail.com>
2023-06-26 09:46:35 +0700

    cso

--- src/features/dokumen/register-kbli-api-slice.ts
+++ src/features/dokumen/register-kbli-api-slice.ts
@@ -1,153 +1,72 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { baseRestAPIUrl } from "../config/config";
 import { IRegisterKbli } from "../entity/register-kbli";
+import { IQueryParamFilters, qFilters } from "../entity/query-param-filters";
+import { baseQueryWithReauth } from "../config/helper-function";
 
 type daftarRegisterKbli = IRegisterKbli[];
 
 export const RegisterKbliApiSlice = createApi({
     reducerPath: 'registerKbliApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: baseRestAPIUrl,
-    }),
+    baseQuery: baseQueryWithReauth,
     refetchOnReconnect: true,
-    keepUnusedDataFor: 30,
-    tagTypes:['RegisterKbli', 'RegisterKbliPage', 'RegisterKbliNib', 'RegisterKbliNibPage', 'RegisterKbliKode', 'RegisterKbliKodePage', 'RegisterKbliNama', 'RegisterKbliNamaPage'],
+    tagTypes:['RegisterKbli'],
     endpoints(builder) {
         return {
-            addRegisterKbli: builder.mutation<IRegisterKbli, Partial<IRegisterKbli>>({
+            save: builder.mutation<IRegisterKbli, Partial<IRegisterKbli>>({
                 query: (body) => ({
                     url: 'register_kbli',
                     method: 'POST',
                     body,
                 }),
-                invalidatesTags: [{type: 'RegisterKbli', id: 'LIST'}, {type: 'RegisterKbliPage', id: 'LIST'}, {type: 'RegisterKbliNib', id: 'LIST'}, {type: 'RegisterKbliNibPage', id: 'LIST'}, {type: 'RegisterKbliKode', id: 'LIST'}, {type: 'RegisterKbliKodePage', id: 'LIST'}, {type: 'RegisterKbliNama', id: 'LIST'}, {type: 'RegisterKbliNamaPage', id: 'LIST'}]
+                invalidatesTags: [{type: 'RegisterKbli', id: 'LIST'}]
             }),
-            updateRegisterKbli: builder.mutation<void, {nib: string; kode: string; registerKbli: IRegisterKbli}>({
-                query: ({kode, nib, registerKbli}) => ({
-                    url: `register_kbli/${nib}/${kode}`,
+            update: builder.mutation<void, Partial<IRegisterKbli>>({
+                query: (registerKbli) => ({
+                    url: 'register_kbli',
                     method: 'PUT',
                     body: registerKbli,
                 }),
-                invalidatesTags: (result, error, { nib, kode }) => [{type: 'RegisterKbli', id: `${nib}-${kode}`}, {type: 'RegisterKbliPage', id: `${nib}-${kode}`}, {type: 'RegisterKbliNib', id: `${nib}-${kode}`}, {type: 'RegisterKbliNibPage', id: `${nib}-${kode}`}, {type: 'RegisterKbliKode', id: `${nib}-${kode}`}, {type: 'RegisterKbliKodePage', id: `${nib}-${kode}`}, {type: 'RegisterKbliNama', id: `${nib}-${kode}`}, {type: 'RegisterKbliNamaPage', id: `${nib}-${kode}`}]
+                invalidatesTags: (result, error, {dokumenNibOss, kbli}) => [{type: 'RegisterKbli', id: `${dokumenNibOss?.nomor}-${kbli?.kode}`},]
             }),
-            deleteRegisterKbli: builder.mutation<{ success: boolean; id: string }, {nib: string; kode: string}>({
-                query({nib, kode} ) {
-                  return {
-                    url: `register_kbli/${nib}/${kode}`,
+            updateId: builder.mutation<void, {id: string; registerKbli: IRegisterKbli}>({
+                query: ({id, registerKbli}) => ({
+                    url: `register_kbli/id/${id}`,
+                    method: 'PUT',
+                    body: registerKbli,
+                }),
+                invalidatesTags: (result, error, { id }) => {
+                    return [{type: 'RegisterKbli', id}];
+                }
+            }),
+            delete: builder.mutation<Partial<IRegisterKbli>, Partial<IRegisterKbli>>({
+                query: (registerKbli) => ({                  
+                    url: 'register_kbli',
                     method: 'DELETE',
-                  }
-                },
-                invalidatesTags: (result, error, {nib, kode}) => [{type: 'RegisterKbli', id: `${nib}-${kode}`}, {type: 'RegisterKbliPage', id: `${nib}-${kode}`}, {type: 'RegisterKbliNib', id: `${nib}-${kode}`}, {type: 'RegisterKbliNibPage', id: `${nib}-${kode}`}, {type: 'RegisterKbliKode', id: `${nib}-${kode}`}, {type: 'RegisterKbliKodePage', id: `${nib}-${kode}`}, {type: 'RegisterKbliNama', id: `${nib}-${kode}`}, {type: 'RegisterKbliNamaPage', id: `${nib}-${kode}`}]
+                    body: registerKbli,                
+                }),
+                invalidatesTags: (result, error, { dokumenNibOss, kbli }) => [{type: 'RegisterKbli', id: `${dokumenNibOss?.nomor}-${kbli?.kode}`},]
             }),
-            getAllRegisterKbli: builder.query<daftarRegisterKbli, void>({
-                query: () => `register_kbli`,
+            getDaftarData: builder.query<daftarRegisterKbli, IQueryParamFilters>({
+                query: (queryParams) => `register_kbli?filters=${JSON.stringify(queryParams)}`,
                 providesTags: (result) => 
                     result ?
                     [
                         ...result.map(
-                            ({ idNib, idKbli }) => ({ type: 'RegisterKbli' as const, id: `${idNib}-${idKbli}` })
+                            ({dokumenNibOss, kbli}) => ({ type: 'RegisterKbli' as const, id: `${dokumenNibOss?.nomor}-${kbli?.kode}`})
                         ),
                         { type: 'RegisterKbli', id: 'LIST' },
                     ]:
                     [{type: 'RegisterKbli', id: 'LIST'}],
             }),
-            getRegisterKbliByPage: builder.query<daftarRegisterKbli, {page: number; pageSize: number}>({
-                query: ({page = 1, pageSize = 10}) => `register_kbli/page?page=${page}&pageSize=${pageSize}`,
-                providesTags: (result) => 
-                    result ?
-                    [
-                        ...result.map(
-                            ({ idNib, idKbli }) => ({ type: 'RegisterKbli' as const, id: `${idNib}-${idKbli}` })
-                        ),
-                        { type: 'RegisterKbli', id: 'LIST' },
-                    ]:
-                    [{type: 'RegisterKbli', id: 'LIST'}],
-            }),
-            getRegisterKbliByNama: builder.query<daftarRegisterKbli, string>({
-                query: (nama) => `register_kbli/nama?nama=${nama}`,
-                providesTags: (result) => 
-                    result ?
-                    [
-                        ...result.map(
-                            ({ idNib, idKbli }) => ({ type: 'RegisterKbliNama' as const, id: `${idNib}-${idKbli}` })
-                        ),
-                        { type: 'RegisterKbliNama', id: 'LIST' },
-                    ]:
-                    [{type: 'RegisterKbliNama', id: 'LIST'}],
-            }),
-            getRegisterKbliByNamaAndPage: builder.query<daftarRegisterKbli, {nama: string; page: number; pageSize: number}>({
-                query: ({nama = '', page=1, pageSize=10}) => `register_kbli/nama/page?nama=${nama}&page=${page}&pageSize=${pageSize}`,
-                providesTags: (result) => 
-                    result ?
-                    [
-                        ...result.map(
-                            ({ idNib, idKbli }) => ({ type: 'RegisterKbliNamaPage' as const, id: `${idNib}-${idKbli}` })
-                        ),
-                        { type: 'RegisterKbliNamaPage', id: 'LIST' },
-                    ]:
-                    [{type: 'RegisterKbliNamaPage', id: 'LIST'}],
-            }),
-            getRegisterKbliByKode: builder.query<daftarRegisterKbli, string>({
-                query: (kode) => `register_kbli/kode?kode=${kode}`,
-                providesTags: (result) => 
-                    result ?
-                    [
-                        ...result.map(
-                            ({ idNib, idKbli }) => ({ type: 'RegisterKbliKode' as const, id: `${idNib}-${idKbli}` })
-                        ),
-                        { type: 'RegisterKbliKode', id: 'LIST' },
-                    ]:
-                    [{type: 'RegisterKbliKode', id: 'LIST'}],
-            }),
-            getRegisterKbliByKodeAndPage: builder.query<daftarRegisterKbli, {kode: string; page: number; pageSize: number}>({
-                query: ({kode = '', page=1, pageSize=10}) => `register_kbli/kode/page?kode=${kode}&page=${page}&pageSize=${pageSize}`,
-                providesTags: (result) => 
-                    result ?
-                    [
-                        ...result.map(
-                            ({ idNib, idKbli }) => ({ type: 'RegisterKbliKodePage' as const, id: `${idNib}-${idKbli}` })
-                        ),
-                        { type: 'RegisterKbliKodePage', id: 'LIST' },
-                    ]:
-                    [{type: 'RegisterKbliKodePage', id: 'LIST'}],
-            }),
-            getRegisterKbliByNib: builder.query<daftarRegisterKbli, string>({
-                query: (nib) => `register_kbli/nib?nib=${nib}`,
-                providesTags: (result) => 
-                    result ?
-                    [
-                        ...result.map(
-                            ({ idNib, idKbli }) => ({ type: 'RegisterKbliNib' as const, id: `${idNib}-${idKbli}` })
-                        ),
-                        { type: 'RegisterKbliNib', id: 'LIST' },
-                    ]:
-                    [{type: 'RegisterKbliNib', id: 'LIST'}],
-            }),
-            getRegisterKbliByNibAndPage: builder.query<daftarRegisterKbli, {nib: string; page: number; pageSize: number}>({
-                query: ({nib = '', page=1, pageSize=10}) => `register_kbli/nib/page?kode=${nib}&page=${page}&pageSize=${pageSize}`,
-                providesTags: (result) => 
-                    result ?
-                    [
-                        ...result.map(
-                            ({ idNib, idKbli }) => ({ type: 'RegisterKbliNibPage' as const, id: `${idNib}-${idKbli}` })
-                        ),
-                        { type: 'RegisterKbliNibPage', id: 'LIST' },
-                    ]:
-                    [{type: 'RegisterKbliNibPage', id: 'LIST'}],
+            getJumlahData: builder.query<number, qFilters>({
+                query: (queryFilters) => `register_kbli/count?filters=${JSON.stringify(queryFilters)}`,
             }),
         }
     }
 });
 
 export const { 
-    useAddRegisterKbliMutation, 
-    useUpdateRegisterKbliMutation, 
-    useDeleteRegisterKbliMutation,
-    useGetAllRegisterKbliQuery,
-    useGetRegisterKbliByPageQuery,
-    useGetRegisterKbliByNamaQuery,
-    useGetRegisterKbliByKodeQuery,
-    useGetRegisterKbliByKodeAndPageQuery,
-    useGetRegisterKbliByNibQuery,
-    useGetRegisterKbliByNibAndPageQuery,
+    useSaveMutation, useUpdateMutation,
+    useUpdateIdMutation, useDeleteMutation,
+    useGetDaftarDataQuery, useGetJumlahDataQuery
 } = RegisterKbliApiSlice;
